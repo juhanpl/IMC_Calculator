a5deb0d01efcef3fa4111a1c364969306840dc6e
Juan <mendozalopez.py@gmail.com>
2025-11-26 15:22:43 -0500

    subir proyecto

---
+++ app/src/main/java/com/example/miprimerappp/IMCActivity.kt
@@ -0,0 +1,18 @@
+package com.example.miprimerappp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
+
+class IMCActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc)
+
+
+
+    }
+}
